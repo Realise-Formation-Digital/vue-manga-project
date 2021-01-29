218c2c0b7b09d4f1f5aef252fff3ec5d35aa7f7f
Elisa Solazzo <elisa.solazzo@gmail.com>
2021-01-29 11:28:20 +0100

    Missing instructions

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import Mangas from '@/components/Mangas'
 import Home from '@/components/Home'
 import Contact from '@/components/Contact'
 import vuetify from '@/plugins/vuetify.js' // path to vuetify export
+import 'vuetify/dist/vuetify.min.css';
 
+Vue.use(Vuetify)
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
@@ -31,6 +33,6 @@ const router = new VueRouter({
 
 new Vue({
   render: h => h(App),
-vuetify
+  vuetify,
   router: router, // la valeur doit être le nom de la constante de l'objet VueRouter instancié
 }).$mount('#app')
