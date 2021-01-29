7f46a8449bba592c143059856cc3cee47c9a9622
Anton <grekov.anton96@gmail.com>
2021-01-29 16:58:12 +0100

    manga

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+v-btn{
+  color:black
+}
 </style>
--- src/components/mangas.vue
+++ src/components/mangas.vue
@@ -3,8 +3,41 @@
    <div>
 <div class ="divv" v-for="(item, index) in table" :key="index" @click="show(item)">
   <img class="image" :src="item.image_url">
-  <p>{{item.title}}</p>
+  <h4>{{item.title}}</h4>
 </div>
+
+<div data-app>
+      <v-dialog
+        transition="dialog-top-transition"
+        max-width="600"
+      >
+        <template v-slot:activator="{  on, attrs }">
+        <v-btn
+            color="primary"
+            v-bind="attrs"
+            v-on="on"
+          ></v-btn> 
+        </template>
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar
+              color="primary"
+              dark
+            >Opening from the top</v-toolbar>
+            <v-card-text>
+              <div class="text-h2 pa-12">Hello world!</div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn
+                text
+                @click="dialog.value = false"
+              >Close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+</div>
+
 </div>
 </template>
 
@@ -12,20 +45,32 @@
 
 // Make a request for a user with a given ID
 export default {
-  name: 'beers',
+  name: 'mangas',
   props: {
     messageToShow: Object
   },
+  async mounted(){
+    this.table = this.getTable()
+  },
   data() {
     return {
-      table: this.getTable(),
-      selectedItem: null
+      table: [],
+      selectedItem: null,
+      dialog: false
     }
   },
   methods: {
 async getTable() {
   try{
+
+    
     const axios = require('axios');
+
+
+//axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+//, {headers: {"Access-Control-Allow-Origin": "*"}}
+
+
     const result = await axios.get("https://api.jikan.moe/v3/top/manga/1/bypopularity");
     console.log(result.data);
     this.table = result.data.top
@@ -55,8 +100,8 @@ show(item) {
 }
 
 .divv{
-    width: 235px;
-    height: 230px;
+    width: 170px;
+    height: 270px;
     vertical-align: top;
     display: inline-block;
     text-align: center;
@@ -70,7 +115,6 @@ show(item) {
 }
 
 .image {
-    width: 235px;
     height: 200px;
     vertical-align: top;
     display: inline-block;
