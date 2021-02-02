6ba3b86ee71ee2039fe46195fdf108df4c5d757f
Your <christophermoret@gmail.com>
2021-02-02 21:06:32 +0100

    .

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,12 +2,12 @@
   <div>
     <v-row>
       <v-col
-        v-for="item in items"
-        :key="item.id"
+        v-for="image in images"
+        :key="image.id"
         class="d-flex child-flex"
         cols="4"
       >
-        <v-img :src="item.src" aspect-ratio="2" class="grey lighten-2">
+        <v-img :src="image.src" aspect-ratio="2" class="grey lighten-2">
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
               <v-progress-circular
@@ -19,27 +19,16 @@
         </v-img>
       </v-col>
     </v-row>
+
     <br />
     <br />
-    <v-row>
-      <v-btn
-        v-for="item in items"
-        :key="item.id"
-        class="d-flex child-flex"
-        cols="4"
-      >
-        <v-img :src="item.src" aspect-ratio="2" class="grey lighten-2">
-          <template v-slot:placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular
-                indeterminate
-                color="grey lighten-5"
-              ></v-progress-circular>
-            </v-row>
-          </template>
-        </v-img>
-      </v-btn>
-    </v-row>
+
+    <div>
+      <iframe src="https://api.jikan.moe/v3/anime/1" width="1500" height="800"></iframe>
+    </div>
+    
+
+
     <br />
     <br />
   </div>
@@ -49,7 +38,7 @@
 export default {
   data() {
     return {
-      items: [
+      images: [
         {
           id: 1,
           src: require("../assets/theworldo.gif"),
@@ -63,6 +52,21 @@ export default {
           src: require("../assets/katon.gif"),
         },
       ],
+      items: "https://api.jikan.moe/v3/person/1/pictures",
+      categories: [ 
+                { 
+                    name: 'Popular',
+                    url: 'https://api.jikan.moe/v3/top/anime/1/bypopularity'
+                },
+                {
+                    name: "Community's favourite",
+                    url: 'https://api.jikan.moe/v3/top/anime/1/favorite'
+                },
+                {
+                    name: 'Upcoming',
+                    url: 'https://api.jikan.moe/v3/top/anime/1/upcoming'
+                }
+            ],
     };
   },
 };
