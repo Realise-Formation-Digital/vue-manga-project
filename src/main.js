f21f53d3ebc509248c699a040bc87ca67a56aa0b
daniel <danimahta1@gmail.com>
2021-02-02 13:57:37 +0100

    Revert "Merge pull request #15 from Realise-Formation-Digital/Anton"
    This reverts commit 2609a408d223fa741d5a94e65a673064224163fd, reversing
    changes made to 5b0a833f855ec6623faa1a790385e90d5d029c79.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,54 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-import vuetify from '@/plugins/vuetify' // path to vuetify export
+import vuetify from '@/plugins/vuetify.js' // path to vuetify export
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+
+
+// Importing parts
+// import Footer from '@components/Footer'.vue
+
+// Importing pages
+import Mangas from '@/components/Mangas'
+import Animes from '@/components/Animes'
+import Home from '@/components/Home'
+import Contact from '@/components/Contact'
+
+// import 'vuetify/dist/vuetify.min.css';
+
+// Vue.use(Vuetify)
+Vue.use(VueAxios, axios)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/mangas',
+    component: Mangas
+  },
+  {
+    path: '/animes',
+    component: Animes
+  },
+  { 
+    path: '/contact',
+    component: Contact
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+})
+
 new Vue({
-render: h => h(App),
-vuetify
-}).$mount('#app');
+  render: h => h(App),
+  vuetify,
+  router: router, // la valeur doit être le nom de la constante de l'objet VueRouter instancié
+}).$mount('#app')
