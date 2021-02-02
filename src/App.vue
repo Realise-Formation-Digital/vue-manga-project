acba73bd9622babc113bfa78f1c281714c733a4e
Chris <christophermoret@gmail.com>
2021-02-02 10:44:05 +0100

    Vuetify Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="grey lighten-4">
     <NavBar/>
     <router-view/>
     <Footer/>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,23 @@
 <template>
-  <v-card>
-    <img src="../assets/animania.png" alt="Header with pictures of characters from animes and mangas" style="width: 100%">
-        <v-toolbar>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-          <v-toolbar-title>Welcome to Animania</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <router-link to="/">Home Page</router-link>
-          <v-spacer></v-spacer>
-          <router-link to="/mangas">Mangas</router-link>
-          <v-spacer></v-spacer>
-          <router-link to="/animes">Animes</router-link>
-          <v-spacer></v-spacer>
-          <router-link to="/aboutus">About Us</router-link>
-        </v-toolbar>
-  </v-card>
+  <nav>
+    <v-tollbar flat app>
+      <v-tool-bar-title class="text-uppercasegrey--text">
+        <span class="font-weight-light">Welcome to Animanga</span>
+      </v-tool-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn router-link to ="/">Home Page</v-btn>
+      <v-btn router-link to ="/mangas">Manga</v-btn>
+      <v-btn router-link to ="/animes">Anime</v-btn>
+      <v-btn router-link to ="/aboutus">About Us</v-btn>
+    </v-tollbar>
+  </nav>
 </template>
 
 
+<script>
+  export default {
+    data: () => ({
+      collapseOnScroll: true,
+    }),
+  }
+</script>
