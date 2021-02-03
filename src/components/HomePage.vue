5e4fb7779f1aaf3d66401c53d3181b3fb8c4d126
Chris <christophermoret@gmail.com>
2021-02-03 14:02:00 +0100

    Clean Marco 2

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -23,14 +23,33 @@
     <br />
     <br />
 
-     <v-btn
-      color="#F44336"
-      @click="fetchmanga()"
-    >
-      Primary
-    </v-btn>
-   
+    <v-btn color="#9FA8DA" @click="fetchmanga()"> Mangas List </v-btn>
 
+    <v-btn color="#90CAF9" @click="fetchmanga()"> Animes List </v-btn>
+
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Character Name</th>
+            <th class="text-left">Role</th>
+            <th class="text-left">Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(character, i) in characters" :key="i">
+            <td>{{ character.name }}</td>
+            <td>{{ character.role }}</td>
+            <td> <v-avatar>
+      <img
+        :src="character.image_url"
+        alt="John"
+      >
+    </v-avatar></td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
 
     <br />
     <br />
@@ -39,6 +58,8 @@
 
 <script>
 export default {
+  //Data me permet de declarer mes variables pour ce composant
+  // Ici un array s appel par list, par ex Mangas
   data() {
     return {
       images: [
@@ -55,20 +76,23 @@ export default {
           src: require("../assets/katon.gif"),
         },
       ],
+      characters: [],
     };
   },
   methods: {
-    async fetchmanga(){
-      try{
-        const axios = require('axios')
-        const response = await axios.get('https://api.jikan.moe/v3/manga/1/characters');
+    async fetchmanga() {
+      try {
+        const axios = require("axios");
+        const response = await axios.get(
+          "https://api.jikan.moe/v3/manga/1/characters"
+        );
         console.log(response);
-
-      }catch(e){
-        console.log("error:",e)
+        this.characters = response.data.characters;
+      } catch (e) {
+        console.log("error:", e);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
