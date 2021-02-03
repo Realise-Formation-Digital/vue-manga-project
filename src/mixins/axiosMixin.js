cff2e83d070876e48c38ce52305cee5e85bbe52b
Dario <dariogallego2420@gmail.com>
2021-02-03 13:37:03 +0100

    last commit before merge with main

---
+++ src/mixins/axiosMixin.js
@@ -0,0 +1,28 @@
+// Defining axiosMixin object --> Should the various const be part of it?
+export default {
+    methods: {
+        hello() {
+            console.log("Hello and good job")
+        },
+        async show(url, top) {
+            try {
+            const axios = require ('axios')
+            const response = await axios.get(url) 
+            top === true ? this.result = response.data.top : this.result = response.data.anime
+            console.log("Data fetched and displayed on page");
+            } catch (e) {
+                console.error(e)
+            }
+        },
+        async miyazaki() {
+            try {
+            const axios = require ('axios')
+            const response = await axios.get('https://api.jikan.moe/v3/person/1870') 
+            this.result = response.data.anime_staff_positions.anime
+            console.log("Data fetched and displayed on page" + this.result);
+            } catch (e) {
+                console.error(e)
+            }
+        }
+    }
+}
